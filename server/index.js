a92411f409d0a459598680efc05707ba380d5b2b
Toan Tran <trantoankk@gmail.com>
2020-05-15 15:36:50 -0700

    dockerized proxy server and ready to deploy to EC2

--- server/index.js
+++ server/index.js
@@ -16,31 +16,31 @@ app.use('/', (req, res, next) => {
 app.use(express.static(path.join(__dirname, '../public/')));
 
 app.get('/rooms/:roomID/reviews', (req, res) => {
-  axios.get('http://localhost:3001/rooms/2/reviews')
+  axios.get('http://18.220.252.131:3001/rooms/2/reviews')
   .then(result => res.status(200).send(result.data))
   .catch(err => console.log(err));
 });
 
 app.get('/rooms/:roomID/reviews/:phrase', (req, res) => {
-  axios.get(`http://localhost:3001/rooms/2/reviews/${req.params.phrase}`)
+  axios.get(`http://18.220.252.131:3001/rooms/2/reviews/${req.params.phrase}`)
   .then(result => res.status(200).send(result.data))
   .catch(err => console.log(err));
 });
 
 app.get('/carousel', (req, res) => {
-  axios.get(`http://localhost:3003/carousel`)
+  axios.get(`http://13.57.183.102:3003/carousel`)
   .then(result => res.status(200).send(result.data))
   .catch(err => console.log(err));
 });
 
 app.get('/rooms', (req, res) => {
-  axios.get(`http://localhost:3004/rooms`)
+  axios.get(`http://3.101.29.123:3004/rooms`)
   .then(result => res.status(200).send(result.data))
   .catch(err => console.log(err));
 });
 
 app.get('/data', (req, res) => {
-  axios.get(`http://localhost:3002/data`)
+  axios.get(`http://18.217.34.96:3002/data`)
   .then(result => res.status(200).send(result.data))
   .catch(err => console.log(err));
 });
