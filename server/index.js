58fa547a557cbe7f0bc988644cf1f0d30d44697a
Toan Tran <trantoankk@gmail.com>
2020-05-20 16:44:08 -0700

    Replaced all services IP

--- server/index.js
+++ server/index.js
@@ -16,19 +16,19 @@ app.use('/', (req, res, next) => {
 app.use(express.static(path.join(__dirname, '../public/')));
 
 app.get('/rooms/:roomID/reviews', (req, res) => {
-  axios.get('http://18.220.252.131:3001/rooms/2/reviews')
+  axios.get('http://18.222.212.45:3001/rooms/2/reviews')
   .then(result => res.status(200).send(result.data))
   .catch(err => console.log(err));
 });
 
 app.get('/rooms/:roomID/reviews/:phrase', (req, res) => {
-  axios.get(`http://18.220.252.131:3001/rooms/2/reviews/${req.params.phrase}`)
+  axios.get(`http://18.222.212.45:3001/rooms/2/reviews/${req.params.phrase}`)
   .then(result => res.status(200).send(result.data))
   .catch(err => console.log(err));
 });
 
 app.get('/carousel', (req, res) => {
-  axios.get(`http://13.57.183.102:3003/carousel`)
+  axios.get(`http://18.144.126.16:3003/carousel`)
   .then(result => res.status(200).send(result.data))
   .catch(err => console.log(err));
 });
